c738aa0a256c24bedc0abecc7f1ccf77dc719b0a
Alfonso Rodríguez Pereira <arodriguez@teltek.es>
2020-05-28 01:54:02 +0200

    TTK-23454 api: Created APISeries endpoint to create a series

---
+++ Controller/APISeriesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Pumukit\ExternalAPIBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/api/series")
+ * @Security("is_granted('ROLE_ACCESS_INGEST_API')")
+ */
+class APISeriesController extends Controller
+{
+    /**
+     * @Route("", methods="POST")
+     */
+    public function createAction(Request $request): ?Response
+    {
+        $apiSeriesService = $this->get('pumukit_external_api.api_series_service');
+        $title = $request->request->get('title');
+        $series = $apiSeriesService->create($this->getUser(), $title);
+
+        return new Response($series->getId());
+    }
+}
