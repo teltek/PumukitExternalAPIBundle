6ed54191398115e61c5d9f9252896dbe3a2c7e1b
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2024-02-06 09:08:48 +0100

    Update DOC, and add github actions

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,6 +1,7 @@
 <?php
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
+    ->exclude('var')
 ;
 
 $config =  new PhpCsFixer\Config();
@@ -18,6 +19,6 @@ return $config->setRules([
         'sort_algorithm' => 'none',
     ],
 ])
-->setFinder($finder)
-->setRiskyAllowed(true)
-;
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+    ;
